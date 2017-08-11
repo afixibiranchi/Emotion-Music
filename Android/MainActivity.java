69efcbe753b6d85c08edce7e7f8b03103f63b6ce
lingz1994 <lingz1994@users.noreply.github.com>
2017-08-12 00:49:43 +0800

    Manage to play music

--- Android/MainActivity.java
+++ Android/MainActivity.java
@@ -1,12 +1,26 @@
 package com.example.lingez.firstapp;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import org.json.JSONObject;
+
+import java.io.File;
 
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_sad;
+    private Button btn_hpy;
+    private Button btn_ang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +28,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         conn();
+
+        btn_sad = (Button) findViewById(R.id.btn_sad);
+        btn_hpy = (Button) findViewById(R.id.btn_hpy);
+        btn_ang = (Button) findViewById(R.id.btn_ang);
+
+       btn_ang.setOnClickListener(
+               new View.OnClickListener() {
+                   @Override
+                   public void onClick(View v) {
+                       Intent intent = new Intent();
+                       intent.setAction(android.content.Intent.ACTION_VIEW);
+                       String filepath = Environment.getExternalStoragePublicDirectory((Environment.DIRECTORY_MUSIC)).toString();
+                       System.out.println(filepath);
+                       filepath = filepath.concat("/Scream.mp3");
+                       File file = new File(filepath);
+                       Log.d("Error", filepath);
+                       intent.setDataAndType(Uri.fromFile(file), "audio/*");
+                       startActivity(intent);
+                   }
+               }
+       );
     }
 
     public void conn() {
@@ -30,40 +65,21 @@ public class MainActivity extends AppCompatActivity {
 
             });
 
-            //socket.on("incoming", new Emitter.Listener() {
+            socket.on("incoming", new Emitter.Listener() {
 
-//                @Override
-//                public void call(final Object... args) {
-//                    Log.d(TAG, args.toString());
-//                    JSONObject jsonObject = (JSONObject) args[0];
-//                    String x = null;
-//                    int y = 0;
-//                    try {
-//                        x = (String) jsonObject.get("test");
-//                        y = (int) jsonObject.get("time");
-//                    } catch (JSONException e) {
-//                        e.printStackTrace();
-//                    }
-//                    System.out.println(jsonObject.toString());
-//                    System.out.println(x);
-//                    if(x.equals("forward")){
-//                        send2Server("F", y);
-//                    }else if(x.equals("backward")){
-//                        send2Server("B", y);
-//                    }else if(x.equals("right")){
-//                        send2Server("R", y);
-//                    }else if(x.equals("left")){
-//                        send2Server("L", y);
-//                    }
-//                }
+                @Override
+                public void call(final Object... args) {
+                    Log.d("Error", args.toString());
+                    JSONObject jsonObject = (JSONObject) args[0];
+                }
 
-//            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
-//
-//                @Override
-//                public void call(Object... args) {
-//                    //Log.d(TAG, "Disconnected");
-//                }
-//            });
+            }).on(Socket.EVENT_DISCONNECT, new Emitter.Listener() {
+
+                @Override
+                public void call(Object... args) {
+                    //Log.d(TAG, "Disconnected");
+                }
+            });
 
             socket.connect();
         }catch (Exception e){
